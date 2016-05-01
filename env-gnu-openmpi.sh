d0f2dc4e9552d086fa0d06661160f9fcb8bd0b21
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2016-05-01 21:46:08 +0900

    Fix: delete unuse files

--- env-gnu-openmpi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source env-gnu.sh
-
-export MY_MPI_PREFIX="${MY_PREFIX}/openmpi"
-export MPI_BASE_DIR=${HOME}/local/gnu/openmpi
-export MPICC=${HOME}/local/gnu/openmpi/bin/mpicc
-export MPIFC=${HOME}/local/gnu/openmpi/bin/mpif90
-
